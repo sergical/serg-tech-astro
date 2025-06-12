7997cdd3b7bfbd8acb141efa1d505e342ae271f3
Sergiy Dybskiy <s@serg.tech>
2025-06-12 10:02:42 -0400

    migrate and update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
-
 import cloudflare from "@astrojs/cloudflare";
 
 import svelte from "@astrojs/svelte";
 
 import react from "@astrojs/react";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind(), svelte(), react()],
+  integrations: [svelte(), react()],
   adapter: cloudflare(),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
